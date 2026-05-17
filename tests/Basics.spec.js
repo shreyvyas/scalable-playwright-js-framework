2cc97feda636233fbea267a04b09e75f8d6ff7f3
shreyvyas <shreyvyas@gmail.com>
2026-05-17 23:28:02 +0530

    refactor: update Playwright configuration and add new test cases

---
+++ tests/Basics.spec.js
@@ -0,0 +1,127 @@
+import {test, expect} from '@playwright/test';
+
+test("Check Page Title", async ({page}) => {
+
+    await page.goto("https://web-locators-static-site-qa.vercel.app/");
+
+    const title = await page.title();
+
+    console.log(title);
+
+    await expect(page).toHaveTitle(title);
+    
+})
+
+test("Handle Edit Box", async ({page}) => {
+
+    await page.goto("https://web-locators-static-site-qa.vercel.app/");
+
+    await page.getByText("Edit").click();
+    
+    await page.getByPlaceholder("White").fill("Max and Joy");
+
+    await page.pause();
+})
+
+
+test("Is Editable", async ({page}) => {
+
+    await page.goto("https://web-locators-static-site-qa.vercel.app/");
+
+    await page.getByText("Edit").click();
+    
+    const isEditable = await page.getByPlaceholder("55");
+
+    let result = await isEditable.isEditable();
+
+    console.log(result);
+    
+})
+
+test("Button Handling", async ({page})=> {
+
+    await page.goto("https://web-locators-static-site-qa.vercel.app/");
+
+    await page.getByText("Button").click();
+
+    await page.getByText("Click this button").click();
+
+    const text = await page.getByText("Thank you for clicking me.You may click the button below to bring me back.");
+
+    console.log(await text.textContent());
+
+    await expect(text).toBeVisible();
+    
+})
+
+
+test("Handle Hyperlink", async ({page})=> {
+
+    await page.goto("https://web-locators-static-site-qa.vercel.app/");
+
+    await page.getByText("Hyperlink").click();
+
+    //await page.getByText("Crio.do").click();
+
+    //const isClickable = await page.getByText("Crio.do");
+
+    const isClickable = await page.getByText("Crio.do");
+
+    let result = await isClickable.isEnabled();
+
+    console.log(result);
+
+    await page.pause();
+
+})
+
+test("Check <a> tag", async({page})=> {
+
+    await page.goto("https://web-locators-static-site-qa.vercel.app/");
+
+    await page.getByText("Hyperlink").click();
+
+    const tagname = await page.getByText(" Non - Clickable ").evaluate(el => el.tagName);
+
+    console.log(tagname);
+
+    await expect(tagname).toBe("A");
+
+})
+
+test("Image", async ({page}) => {
+
+    await page.goto("https://web-locators-static-site-qa.vercel.app/");
+
+    await page.getByText("Image").click();
+    
+})
+
+test("Dropdown Handling", async ({page}) => {
+
+    await page.goto("https://web-locators-static-site-qa.vercel.app/");
+
+    await page.getByText("Dropdown").click();
+
+    const dropdown1 = page.locator(".Dropdown_section1 select.dropdown");
+
+    await dropdown1.selectOption({value: "Greece"});
+    
+})
+
+test("Multi Options Dropdown", async ({page}) => {
+
+    await page.goto("https://web-locators-static-site-qa.vercel.app/");
+
+    await page.getByText("Dropdown").click();
+
+    await page.getByRole('tab', {name: 'Multi Select Dropdown'}).click();
+
+    const locator = page.locator(".DropdownMulti_section1 select.multi");
+
+    await locator.selectOption(['Adobe InDesign', 'Adobe Photoshop']);
+    
+    await page.pause();
+})
+
+test.only("Handle")
